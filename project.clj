c5a65d6ee8e665ad42b969356f3c5fb126f2b0b3
Sun Ning <nsun@avos.com>
2014-03-28 11:19:12 +0800

    added scm section in project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]])
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
+  :scm {:url "https://github.com/sunng87/papaline"
+        :name "github"})

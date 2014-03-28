6f0adb835712f6863dc6508768529926cf4e72cc
Sun Ning <nsun@avos.com>
2014-03-28 11:12:58 +0800

    release 0.1.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject info.sunng/papaline "0.1.0-SNAPSHOT"
+(defproject info.sunng/papaline "0.1.0"
   :description "Clojure concurrent pipeline on core.async"
   :url "http://github.com/sunng87/papaline"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]])

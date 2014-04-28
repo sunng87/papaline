ecf1e93e4af65193cde5711217f8608530d2293b
Sun Ning <nsun@avos.com>
2014-04-28 11:29:53 +0800

    update async.core to 298

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject info.sunng/papaline "0.1.1"
+(defproject info.sunng/papaline "0.1.2"
   :description "Clojure concurrent pipeline on core.async"
   :url "http://github.com/sunng87/papaline"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
+                 [org.clojure/core.async "0.1.298.0-2a82a1-alpha"]]
   :scm {:url "https://github.com/sunng87/papaline"
         :name "github"})

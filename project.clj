bfb798433b211150a390f479dd6e777cad46ba42
Sun Ning <nsun@avos.com>
2014-04-30 11:17:16 +0800

    update core.async to latest alpha version

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.298.0-2a82a1-alpha"]]
+                 [org.clojure/core.async "0.1.301.0-deb34a-alpha"]]
   :scm {:url "https://github.com/sunng87/papaline"
         :name "github"})

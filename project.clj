4edad280519b29f29f3e1035e2d5c65cb0c8fc28
Sun Ning <nsun@avoscloud.com>
2014-05-19 22:42:11 +0800

    lein-release plugin: preparing 0.3.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject info.sunng/papaline "0.3.1-SNAPSHOT"
+(defproject info.sunng/papaline "0.3.1"
   :description "Clojure concurrent pipeline on core.async"
   :url "http://github.com/sunng87/papaline"
   :license {:name "Eclipse Public License"

c3d293049023b596eff37bcda365cd3687bfb924
Sun Ning <sunng@about.me>
2014-12-06 17:09:12 +0800

    Code refactoring

--- src/papaline/core.clj
+++ src/papaline/core.clj
@@ -45,104 +45,109 @@
   (run-pipeline-timeout [this timeout-interval timeout-val & args])
   (stop! [this]))
 
+(defn- run-task [current-stage ctx error-handler]
+  (let [task (.stage-fn current-stage)
+        args (:args ctx)
+        args (if (or (nil? args) ;; empty arguments
+                     (sequential? args))
+               args [args])]
+    (try
+      (assoc ctx :args (apply task args))
+      (catch Exception e
+        (if (and (instance? clojure.lang.ExceptionInfo e)
+                 (:abort (ex-data e)))
+          (merge ctx (ex-data e))
+          (let [ex (ex-info "Papaline stage error"
+                            {:args args
+                             :stage (:name current-stage)} e)]
+            (when error-handler
+              (error-handler ex))
+            (assoc ctx :ex ex)))))))
+
 (defrecord+ Pipeline [done-chan stages error-handler]
   clojure.lang.IFn
   (invoke [this ctx]
-    (let [entry (-> stages first (.buffer))]
-      (go
-        (>! entry ctx))))
+          (let [entry (-> stages first (.buffer))]
+            (go
+              (>! entry ctx))))
 
   IPipeline
   (start! [this]
-    (loop [stages* stages]
-      (when (first stages*)
-        (let [current-stage (first stages*)
-              in-chan (.buffer current-stage)
-              task (.stage-fn current-stage)
-              out-chan (when (second stages*) (.buffer (second stages*)))]
-          (go-loop []
-            (let [[ctx port] (alts! [done-chan in-chan] :priority true)]
-              (if (not= port done-chan)
-                (do
-                  (go
-                    (let [args (:args ctx)
-                          args (if (or (nil? args) ;; empty arguments
-                                       (sequential? args))
-                                 args [args])
-                          ctx (try
-                                (assoc ctx :args (apply task args))
-                                (catch Exception e
-                                  (if (and (instance? clojure.lang.ExceptionInfo e)
-                                           (:abort (ex-data e)))
-                                    (merge ctx (ex-data e))
-                                    (do
-                                      (when error-handler
-                                        (error-handler (ex-info "Papaline stage error"
-                                                                {:args args
-                                                                 :stage (:name current-stage)} e)))
-                                      (when (:error ctx)
-                                        (>! (:error ctx) e))))))
-                          out-chan (or out-chan (:wait ctx))]
-                      (when out-chan
-                        (cond
-                         (:abort ctx)
-                         (when (:wait ctx) (>! (:wait ctx) ctx))
+          (loop [stages* stages]
+            (when (first stages*)
+              (let [current-stage (first stages*)
+                    in-chan (.buffer current-stage)
+                    out-chan (when (second stages*) (.buffer (second stages*)))]
+                (go-loop []
+                  (let [[ctx port] (alts! [done-chan in-chan] :priority true)]
+                    (if (not= port done-chan)
+                      (do
+                        (go
+                          (let [ctx (run-task current-stage ctx error-handler)
+                                out-chan (or out-chan (:wait ctx))]
+                            (when (and (:error ctx) (:ex ctx))
+                              (>! (:error ctx) (:ex ctx)))
+                            (when out-chan
+                              (cond
+                               (:abort ctx)
+                               (when (:wait ctx) (>! (:wait ctx) ctx))
 
-                         ;; the results are forked
-                         (:fork (meta (:args ctx)))
-                         (let [ctx (-> ctx
-                                       (update-in [:forks]
-                                                  #(conj (or % []) (count @(:args ctx))))
-                                       (update-in  [:fork-rets]
-                                                   #(conj (or % []) (atom []))))]
-                           (doseq [forked-args @(:args ctx)]
-                             (>! out-chan (assoc ctx :args forked-args))))
+                               ;; the results are forked
+                               (:fork (meta (:args ctx)))
+                               (let [ctx (-> ctx
+                                             (update-in [:forks]
+                                                        #(conj (or % []) (count @(:args ctx))))
+                                             (update-in  [:fork-rets]
+                                                         #(conj (or % []) (atom []))))]
+                                 (doseq [forked-args @(:args ctx)]
+                                   (>! out-chan (assoc ctx :args forked-args))))
 
-                         ;; this tasks requires join
-                         (:join (meta (:args ctx)))
-                         (let [fork-rets (swap! (last (:fork-rets ctx)) conj @(:args ctx))]
-                           (when (= (last (:forks ctx)) (count fork-rets))
-                             (>! out-chan (assoc ctx
-                                            :args fork-rets
-                                            :forks (vec (drop-last (:forks ctx)))
-                                            :fork-rets (vec (drop-last (:fork-rets ctx)))))))
+                               ;; this tasks requires join
+                               (:join (meta (:args ctx)))
+                               (let [fork-rets (swap! (last (:fork-rets ctx)) conj @(:args ctx))]
+                                 (when (= (last (:forks ctx)) (count fork-rets))
+                                   (>! out-chan (assoc ctx
+                                                  :args fork-rets
+                                                  :forks (vec (drop-last (:forks ctx)))
+                                                  :fork-rets (vec (drop-last (:fork-rets ctx)))))))
 
-                         ;; normal linear
-                         :else (>! out-chan ctx)))))
-                  (recur))
-                (close! in-chan))))
-          (recur (rest stages*))))))
+                               ;; normal linear
+                               :else (>! out-chan ctx))
+                              )))
+                        (recur))
+                      (close! in-chan))))
+                (recur (rest stages*))))))
 
   (run-pipeline [this & args]
-    (this {:args args}))
+                (this {:args args}))
 
   (run-pipeline-wait [this & args]
-    (let [sync-chan (chan)
-          error-chan (chan)]
-      (this {:args args
-             :wait sync-chan
-             :error error-chan})
-      (let [[result port] (alts!! [done-chan error-chan sync-chan] :priority true)]
-        (condp = port
-          sync-chan (:args result)
-          error-chan (throw result)
-          done-chan (throw (ex-info "Pipeline closed"))))))
+                     (let [sync-chan (chan)
+                           error-chan (chan)]
+                       (this {:args args
+                              :wait sync-chan
+                              :error error-chan})
+                       (let [[result port] (alts!! [done-chan error-chan sync-chan] :priority true)]
+                         (condp = port
+                           sync-chan (:args result)
+                           error-chan (throw result)
+                           done-chan (throw (ex-info "Pipeline closed"))))))
 
   (run-pipeline-timeout [this timeout-interval timeout-val & args]
-    (let [sync-chan (chan)
-          error-chan (chan)
-          timeout-chan (timeout timeout-interval)]
-      (this {:args args
-             :wait sync-chan
-             :error error-chan})
-      (let [[result port] (alts!! [done-chan timeout-chan error-chan sync-chan] :priority true)]
-        (condp = port
-          done-chan (throw (ex-info "Pipeline closed"))
-          sync-chan (:args result)
-          error-chan (throw result)
-          timeout-chan timeout-val))))
+                        (let [sync-chan (chan)
+                              error-chan (chan)
+                              timeout-chan (timeout timeout-interval)]
+                          (this {:args args
+                                 :wait sync-chan
+                                 :error error-chan})
+                          (let [[result port] (alts!! [done-chan timeout-chan error-chan sync-chan] :priority true)]
+                            (condp = port
+                              done-chan (throw (ex-info "Pipeline closed"))
+                              sync-chan (:args result)
+                              error-chan (throw result)
+                              timeout-chan timeout-val))))
   (stop! [this]
-    (>!! done-chan 0)))
+         (>!! done-chan 0)))
 
 (defn pipeline [stages & {:keys [error-handler]}]
   (let [stages (mapv #(if (fn? %) (stage %) %) stages)

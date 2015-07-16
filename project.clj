4e623f4c6e7f07a01f3b04e529f92584d906f739
Ning Sun <sunng@about.me>
2015-07-16 14:23:29 +0800

    Version 0.6.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject info.sunng/papaline "0.6.1-SNAPSHOT"
+(defproject info.sunng/papaline "0.6.1"
   :description "Clojure concurrent pipeline on core.async"
   :url "http://github.com/sunng87/papaline"
   :license {:name "Eclipse Public License"

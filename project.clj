5be044b8a4a16317ac8f065f35f799678ebd28ab
Ning Sun <sunng@about.me>
2017-02-17 17:21:49 +0800

    (feat) update clojure
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/sunng87/papaline"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :scm {:url "https://github.com/sunng87/papaline"
         :name "github"}

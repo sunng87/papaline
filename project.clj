dfc3a8c046237785b25304c7cd23bf6290503259
Ning Sun <sunng@about.me>
2017-12-25 00:18:07 +0800

    (chore) bump versions
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject info.sunng/papaline "0.8.1-SNAPSHOT"
+(defproject info.sunng/papaline "0.9.0-SNAPSHOT"
   :description "Clojure concurrent pipeline on core.async"
   :url "http://github.com/sunng87/papaline"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.3.465"]]
   :scm {:url "https://github.com/sunng87/papaline"
         :name "github"}
   :java-source-paths ["java_src"]
-  :javac-options ["-target" "1.6" "-source" "1.6"]
   :deploy-repositories {"releases" :clojars})
